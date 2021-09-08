697e26698dbaf1680c24341de46724a922ff37b3
lokeshp2902 <lokeshkumar.p@nference.net>
2021-09-08 16:20:21 +0530

    Added subraction test

--- unit_testing/test_ArithmeticOperations.py
+++ unit_testing/test_ArithmeticOperations.py
@@ -5,3 +5,8 @@ from ClassAO import Class_AO
 def test_add_method(x,y,z):
     ao = Class_AO()
     assert(ao.add(x,y) == z)
+
+@pytest.mark.parametrize("x,y,z",[(1,2,1),(1,1,0)])
+def test_sub_method(x,y,z):
+    ao = Class_AO()
+    assert(ao.add(x,y) == z)
