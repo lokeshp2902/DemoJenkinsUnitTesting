4d085265d00ee1630c0d61d543fb4f58b01002c3
lokeshp2902 <lokeshkumar.p@nference.net>
2021-09-08 13:42:25 +0530

    Checking

---
+++ unit_testing/ClassAO.py
@@ -0,0 +1,10 @@
+class Class_AO:
+    def add(self,operand1,operand2):
+        return operand1+operand2
+
+    def sub(self,operand1,operand2):
+        return operand2-operand1
+
+    def mul(self,operand1,operand2):
+        return operand1*operand2
+
