1c0762b2e73ebd3090db804cede2449222eba15a
lokeshp2902 <lokeshkumar.p@nference.net>
2021-09-08 18:02:29 +0530

    ADded coverage config

--- unit_testing/test_ArithmeticOperations.py
+++ unit_testing/test_ArithmeticOperations.py
@@ -9,4 +9,4 @@ def test_add_method(x,y,z):
 @pytest.mark.parametrize("x,y,z",[(1,2,1),(1,1,0)])
 def test_sub_method(x,y,z):
     ao = Class_AO()
-    assert(ao.add(x,y) == z)
+    assert(ao.sub(x,y) == z)
