24d08b51f3102e1957409e7fd55b654dfca294d4
lokeshp2902 <lokeshkumar.p@nference.net>
2021-09-13 19:12:58 +0530

    Deleted conftest.py

--- unit_testing/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-def pytest_addoption(parser):
-    parser.addoption("--PALLADIUM_CONFIG", action="store")
-
-@pytest.fixture
-def PALLADIUM_CONFIG(request):
-    return request.config.getoption("--PALLADIUM_CONFIG")
