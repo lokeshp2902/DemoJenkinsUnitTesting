bb507e15ae59c13b9cad5c13e8e34c115c2e5904
lokeshp2902 <lokeshkumar.p@nference.net>
2021-09-15 11:00:34 +0530

    Added pytest.ini

--- unit_testing/test_ArithmeticOperations.py
+++ unit_testing/test_ArithmeticOperations.py
@@ -1,7 +1,7 @@
 import pytest
 from ClassAO import Class_AO
 
-@pytest.mark.parametrize("x,y,z",[(1,2,3),(1,1,2)])
+@pytest.mark.parametrize("x,y,z",[(1,2,3),(1,1,3)])
 def test_add_method(x,y,z):
     ao = Class_AO()
     assert(ao.add(x,y) == z)
